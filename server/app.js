3430e96dbbb7d4895d982a32199188a96f7d9038
Vricap <vricagedepenggalih@gmail.com>
2025-06-29 18:12:13 +0700

    add cors

--- server/app.js
+++ server/app.js
@@ -1,8 +1,10 @@
 import express from "express";
+import cors from "cors";
 import bodyParser from "body-parser";
 import routes from "./routes/index.js";
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: false })); // parse form data
 
